1a1b8a037bda905f074d9478435c65f227736546
dhunganabibek <Dhunganabibek2016@gmail.com>
2022-12-01 17:21:25 -0600

    Array code added

--- Practice/src/arrays/Main.java
+++ Practice/src/arrays/Main.java
@@ -1,4 +1,15 @@
 package arrays;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] myArray1 = new int[10]; //everything will be zero. size = zero
+
+        int[] myArray2 = new int[] {1,2,3,4,5}; //array of size 5
+
+        for (int j : myArray1) {
+            System.out.println(j);
+        }
+
+
+    }
 }
